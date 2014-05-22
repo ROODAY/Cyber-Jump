75360d98a07470922d81ced79dd5327ea249e915
ROODAY <rudytheninja@gmail.com>
2014-05-22 14:08:54 -0400

    began power up stuff

---
+++ Awesome/PowerUpNode.h
@@ -0,0 +1,26 @@
+//
+//  PowerUpNode.h
+//  Awesome
+//
+//  Created by block7 on 5/22/14.
+//  Copyright (c) 2014 Too Much Daylight. All rights reserved.
+//
+#import <SpriteKit/SpriteKit.h>
+#import "GameObjectNode.h"
+
+typedef NS_ENUM(int, StarType) {
+    STAR_NORMAL,
+    STAR_SPECIAL,
+};
+
+typedef NS_ENUM(int, PowerUpType) {
+    BOOST,
+    JETPACK,
+};
+
+@interface PowerUpNode : GameObjectNode
+
+@property (nonatomic, assign) StarType starType;
+@property (nonatomic, assign) PowerUpType powerUpType;
+
+@end
---
+++ Awesome/PowerUpNode.m
@@ -0,0 +1,13 @@
+//
+//  PowerUpNode.m
+//  Awesome
+//
+//  Created by block7 on 5/22/14.
+//  Copyright (c) 2014 Too Much Daylight. All rights reserved.
+//
+
+#import "PowerUpNode.h"
+
+@implementation PowerUpNode
+
+@end
